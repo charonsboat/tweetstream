f9b63d4b07e9e08611e5180ae0390d76105aedb0
drm2 <davidrmyersii@gmail.com>
2016-10-22 12:58:10 -0400

    Initiating stream and implementing socket.io to emit tweets on the listening connection.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var TwitterBot = require('@drm2/twitterbot');
 var dotenv = require('dotenv');
+var io = require('socket.io');
 
 // load environment vars from file
 dotenv.config();
@@ -10,4 +11,11 @@ var bot = new TwitterBot({
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+var stream = bot.filteredStream(process.env.TWITTER_STREAM_FILTER);
+
+stream.on('tweet', function (tweet)
+{
+    io.emit('tweet', tweet);
 });
